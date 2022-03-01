ae80c5ccf8ba806df2b14684902167031d5bb2f2
Gleidson Leite <gledson.leytte@hotmail.com>
2022-03-01 21:40:01 +0000

    Rendering comments by Status

--- client/src/components/PostList/components/PostComments/index.jsx
+++ client/src/components/PostList/components/PostComments/index.jsx
@@ -1,13 +1,21 @@
-import React from "react"
+import React, { useCallback } from "react"
 
 export default ({ comments }) => {
   
+  const getCommentContent = useCallback((comment) => {
+    const commentContents = {
+      approved: comment.content,
+      pending: "This comment is awaiting moderation",
+      rejected: "This comment has been rejected"
+    }
+    return commentContents[comment.status]
+  }, [])
 
   return (
     <div>
       <ul>
         {comments.map(comment => (
-          <li key={comment.id}>{comment.content}</li>
+          <li key={comment.id}>{getCommentContent(comment)}</li>
         ))}
       </ul>
     </div>
